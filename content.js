63762a5c5bcc11a6e87b1faacad8c37e6d6d7c71
zethporter <zethieboy@gmail.com>
2020-11-21 02:39:00 -0700

    Made little login popup

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
 //Maybe make it so that people can have the option to use right click to save or just click on the save button.
 
+document.getElementById('lgn').addEventListener("click", collecter);
+
+function collecter(){
+    let usr = document.getElementById('usrnme').value;
+    let pwd = document.getElementById('psswrd').value;
+
+    alert("Username: " + usr + "\n" + "Password: " + pwd)
+}
